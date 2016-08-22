34d8241d897bf813c5fbc493bac4b266f5291696
Pavel <motor11chik@yandex.ru>
2016-08-22 23:48:01 +0300

    Add files via upload

--- src/org/sofy/Whiletest.java
+++ src/org/sofy/Whiletest.java
@@ -1,18 +1,45 @@
 package org.sofy;
 
-//: control/Whiletest java
+import java.util.Scanner;
+
 public class Whiletest {
-    private static boolean condition() {
-        double random = Math.random();
-        boolean result = random < 0.99;
-        System.out.print(result + "," + random);
-        return result;
+
+    static String number() {
+        System.out.println("Введите ответ");
+        Scanner in = new Scanner(System.in);
+        String x = in.nextLine();
+        return x;
     }
 
     public static void main(String... args) {
-        while (condition()) {
-            System.out.println("inside 'while' ");
-            System.out.println("Exited 'while' ");
+        int r, q, i;
+        r = 0;
+        q = 0;
+        String[] answere = {
+                "Павел",
+                "Моторный",
+                "22",
+                "Да",
+                "Один"
+        };
+        String[] Queschion = {
+                "Ваше Имя?",
+                "Ваша фамилия?",
+                "Сколько вам лет?",
+                "У вас есть дети?",
+                "Сколько у вас детей?"
+        };
+
+
+        for (i = 0; i < 5; i++) {
+
+            System.out.println(Queschion[i]);
+            if (number().equals(answere[i]))
+                r++;
+            else
+                q++;
         }
+        System.out.println("Количество правильных ответов " + r + " Количество неверных ответов " + q);
     }
+
 }
